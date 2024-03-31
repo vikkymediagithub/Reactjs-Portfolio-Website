6205f2075c45fc897fa04858f5afd207df7b9167
oke victor joseph <okevictor14@gmail.com>
2024-03-31 11:30:52 +0100

    Still on it

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -11,7 +11,7 @@ const Achievement = () => {
     >
       <div>
         <div className="py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2007 - 2010</p>
+          <p className="text-sm text-designColor tracking-[4px]">2019 - 2020</p>
           <h2 className="text-4xl font-bold">Company Experience</h2>
         </div>
         <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
