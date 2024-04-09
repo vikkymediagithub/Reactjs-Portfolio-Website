ac921c100bc861c60e7e7c2ba466cfd75cdbfc99
oke victor joseph <okevictor14@gmail.com>
2024-04-09 03:33:30 +0100

    Done with favicon, banner image, banner content, icon link

--- src/components/banner/Media.jsx
+++ src/components/banner/Media.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaReact } from "react-icons/fa";
-import { SiTailwindcss, SiFigma, SiNextdotjs } from "react-icons/si";
+import { SiTailwindcss, SiFigma, SiGithub, SiNetlify  } from "react-icons/si";
 const Media = () => {
   return (
     <div className="flex flex-col xl:flex-row gap-6 lgl:gap-0 justify-between">
@@ -9,15 +9,15 @@ const Media = () => {
             Find me in
           </h2>
           <div className="flex gap-4">
-            <span className="bannerIcon">
+            <a href="https://web.facebook.com/victor.oke.5458" target="_blank" rel="noopener noreferrer" className="bannerIcon">
               <FaFacebookF />
-            </span>
-            <span className="bannerIcon">
+            </a>
+            <a href="https://twitter.com/ayomide3622" target="_blank" rel="noopener noreferrer" className="bannerIcon">
               <FaTwitter />
-            </span>
-            <span className="bannerIcon">
+            </a>
+            <a href="https://www.linkedin.com/in/oke-victor-14a536261/" target="_blank" rel="noopener noreferrer" className="bannerIcon">
               <FaLinkedinIn />
-            </span>
+            </a>
           </div>
         </div>
         <div>
@@ -25,18 +25,18 @@ const Media = () => {
             BEST SKILL ON
           </h2>
           <div className="flex gap-4">
-            <span className="bannerIcon">
-              <FaReact />
-            </span>
-            <span className="bannerIcon">
-              <SiNextdotjs />
-            </span>
-            <span className="bannerIcon">
+            <a href="https://app.netlify.com/teams/victorjoseph/overview" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+              <SiNetlify />
+            </a>
+            <a href="https://github.com/vikkymediagithub" target="_blank" rel="noopener noreferrer" className="bannerIcon">
+            <SiGithub  />
+            </a>
+            <a href="https://tailwindcss.com/docs/installation" target="_blank" rel="noopener noreferrer" className="bannerIcon">
               <SiTailwindcss />
-            </span>
-            <span className="bannerIcon">
+            </a>
+            <a href="https://www.figma.com/files/recents-and-sharing/recently-viewed?fuid=1223237921336515815" target="_blank" rel="noopener noreferrer" className="bannerIcon">
               <SiFigma />
-            </span>
+            </a>
           </div>
         </div>
       </div>
