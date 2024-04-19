0ab86083642b184fb517e588289d782da6550963
oke victor joseph <okevictor14@gmail.com>
2024-04-19 15:04:16 +0100

    remove animation

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,7 +5,7 @@ const RightBanner = () => {
   return (
     <div className="w-full lgl:w-1/2 flex justify-center items-center relative" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
       <img
-        className="w-[250px] h-[300px] animate-bounce rounded-full p-4 bg-teal-400 lgl:w-[300px] lgl:h-[480px] z-10"
+        className="w-[250px] h-[300px] rounded-full lgl:w-[300px] lgl:h-[480px] z-10  bottom-0 w-[350px] h-[300px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"
         src={bannerImg}
         alt="bannerImg"
       />
