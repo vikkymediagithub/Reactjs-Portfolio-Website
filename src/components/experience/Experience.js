37b7b38db615aaddd6056caed3462c41a32e1872
oke victor joseph <okevictor14@gmail.com>
2024-04-24 14:27:16 +0100

    Checking

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,31 +1,31 @@
-import React from 'react';
+// import React from 'react';
 
-const Experience = () => {
-  return (
-    <section className='py-8'>
-        <div className='container mx-auto px-4'>
-            <h2 className='text-2xl font-semibold bg-gradient-to right from-[#1e2024] to-[#202327] shadow-shadowOne text-white py-2 px-4 rounded uppercase'>Experience</h2>
-            <div className='mt-12 relative before:absolute before:top-0 before:left-10 before:bottom-1 before:w-1 before:bg-white'>
-                <div className='pl-14 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
-                    <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
-                    <p>Graduated At FSS (Federal School of Statistic)</p>
-                </div>
-                <div className='pl-14 mt-12 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
-                    <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
-                    <p>(Junior Frontend Developer)Working at "CLOUDWARE TECHNOLOGIES" as a Frontend Developer</p>
-                </div>
-                <div className='pl-14 mt-12 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
-                    <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
-                    <p>Graduated At FSS (Federal School of Statistic)</p>
-                </div>
-                <div className='pl-14 mt-12 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
-                    <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
-                    <p>Graduated At FSS (Federal School of Statistic)</p>
-                </div>
-            </div>
-        </div>
-    </section>
-  );
-};
+// const Experience = () => {
+//   return (
+//     <section className='py-8'>
+//         <div className='container mx-auto px-4'>
+//             <h2 className='text-2xl font-semibold bg-gradient-to right from-[#1e2024] to-[#202327] shadow-shadowOne text-white py-2 px-4 rounded uppercase'>Experience</h2>
+//             <div className='mt-12 relative before:absolute before:top-0 before:left-10 before:bottom-1 before:w-1 before:bg-white'>
+//                 <div className='pl-14 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
+//                     <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
+//                     <p>Graduated At FSS (Federal School of Statistic)</p>
+//                 </div>
+//                 <div className='pl-14 mt-12 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
+//                     <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
+//                     <p>(Junior Frontend Developer)Working at "CLOUDWARE TECHNOLOGIES" as a Frontend Developer</p>
+//                 </div>
+//                 <div className='pl-14 mt-12 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
+//                     <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
+//                     <p>Graduated At FSS (Federal School of Statistic)</p>
+//                 </div>
+//                 <div className='pl-14 mt-12 relative before:w-4 before:h-3 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-400 before:absolute before:rounded-full before:left-[33px]'>
+//                     <h3 className='absolute -left-1 font-semibold text-lg'>2019</h3>
+//                     <p>Graduated At FSS (Federal School of Statistic)</p>
+//                 </div>
+//             </div>
+//         </div>
+//     </section>
+//   );
+// };
 
-export default Experience;
+// export default Experience;
